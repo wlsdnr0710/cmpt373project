d0f84b33a7af9abcb730e7f9a5d48b63407f37ed
Yi Ching Chou <ycchou@sfu.ca>
2021-01-27 10:59:36 -0800

    Initialize Spring Boot Framework

---
+++ cbr_back_end/src/test/java/com/earth/cbr/CbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earth.cbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
