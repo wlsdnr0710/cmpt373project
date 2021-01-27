b53013599bd6bf54c9fb11fc6efb2c3d056b4016
Yi Ching Chou <ycchou@sfu.ca>
2021-01-27 12:37:52 -0800

    Add three layers code for client APIs
    - Controllers, services, repositories
    - The business logic is not filled in yet
    - This commit is to illustrate the structure of code

---
+++ cbr_back_end/src/main/java/com/earth/cbr/models/Client.java
@@ -0,0 +1,40 @@
+package com.earth.cbr.models;
+
+import javax.persistence.*;
+
+@Entity(name = "Client")
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
