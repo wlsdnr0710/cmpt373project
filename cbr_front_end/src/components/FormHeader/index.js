771a81d4c2f0b32edecab16239026fb1af4ff88a
Yi Ching Chou <ycchou@sfu.ca>
2021-01-30 00:07:15 -0800

    Add FormHeader component
    FormHeader component is a header for forms. We need to pass a header text to this component. The text can be the title of the form.

---
+++ cbr_front_end/src/components/FormHeader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+const FormHeader = ({headerText}) => {
+    return (
+        <div className="form-header">
+            {headerText}
+        </div>
+    );
+};
+
+export default FormHeader;
