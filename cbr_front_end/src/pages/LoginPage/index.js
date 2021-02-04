9178039a4129ef27946c4b26726c699d9c9b5bff
William Tjandra <williamtj2001@gmail.com>
2021-02-04 00:06:30 -0800

    Created axios post and submit/login button

--- cbr_front_end/src/pages/LoginPage/index.js
+++ cbr_front_end/src/pages/LoginPage/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-const LoginPage = () => {
-    return (
-        <div className="loginpage">
-            <p>This is Login Page page.</p>
+import axios from 'axios';
+export default class LoginPage extends Component {
+    constructor(props) {
+        super(props);
 
-            <p>
-                Note that the browser does not refresh the page, but JavaScript re-renders the page.
-                This makes the web looks like an app.
-            </p>
-        </div>
-    );
-};
+        this.state = {
+            username: "",
+            password: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-export default LoginPage;
+    handleSubmit(event) {
+        const { username, password } = this.state;
+
+        axios.post(
+            "http://",
+            {
+                user: {
+                    username: username,
+                    password: password
+                }
+            },
+            { withCredentials: true }
+        )
+            .then(response => {
+                console.log("Logged in response: ", response);
+            })
+            .catch(error => {
+                console.log("Login error: ", error);
+            });
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Status: {this.props.loggedInStatus}</h1>
+                <button onClick={() => this.handleLogoutClick()}>Logout</button>
+
+            </div>
+        )
+    }
+}
+
