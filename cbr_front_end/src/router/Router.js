c054b43afaba84baf02e038a7ed814a8e852d32e
William Tjandra <williamtj2001@gmail.com>
2021-02-05 21:23:39 -0800

    Refactored from comments

--- cbr_front_end/src/router/Router.js
+++ cbr_front_end/src/router/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import LoginPage from "../pages/LoginPage/index";
-import Dashboard from "../pages/Dashboard/index";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
 import NewClient from "../pages/NewClient";
 import ExampleComponent from "../components/ExampleComponent";
 
@@ -9,11 +9,11 @@ const Router = () => {
     return (
         <div className="Router">
             <Switch>
-                <Route path="/loginpage" exact component={LoginPage} />
+                <Route path="/login" exact component={Login} />
                 <Route path="/dashboard" exact component={Dashboard} />
                 <Route path="/new-client" exact component={NewClient} />
                 <Route path="/example" exact component={ExampleComponent} />
-                <Redirect to="/loginpage" />
+                <Redirect to="/login" />
             </Switch>
         </div>
     );
