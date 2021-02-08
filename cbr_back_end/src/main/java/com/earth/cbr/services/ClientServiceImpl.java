ad910199b7d5c7f83de663bd2a0e3898142bbfdf
Hyeonseob Yang <nick_yang_2@sfu.ca>
2021-02-08 00:28:15 -0800

    Change addClient method to include new Client fields

--- cbr_back_end/src/main/java/com/earth/cbr/services/ClientServiceImpl.java
+++ cbr_back_end/src/main/java/com/earth/cbr/services/ClientServiceImpl.java
@@ -29,8 +29,24 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public Client addClient(JSONObject payload) {
-        String clientName = (String) payload.get("name");
-        Client client = new Client(clientName);
+        String firstName = (String) payload.get("firstName");
+        String lastName = (String) payload.get("lastName");
+        Integer birthDate = (Integer) payload.get("birthDate");
+        Character gender = (Character) payload.get("gender");
+        String image = (String) payload.get("image");
+        String zone = (String) payload.get("zone");
+        Integer villageNumber = (Integer) payload.get("villageNumber");
+        Integer signupDate = (Integer) payload.get("signupDate");
+        String contactNumber = (String) payload.get("contactNumber");
+        List<String> disabilities = (List<String>) payload.get("disabilities");
+        Long cbrWorkerId = (Long) payload.get("cbrWorkerId");
+        String caregiverContact = (String) payload.get("caregiverContact");
+        String requiredServices = (String) payload.get("requiredServices");
+        String goals = (String) payload.get("goals");
+
+        Client client = new Client(firstName, lastName,birthDate, gender, image, zone, villageNumber, signupDate,
+                contactNumber, disabilities, cbrWorkerId, caregiverContact, requiredServices, goals);
+        
         return clientRepository.save(client);
     }
 
