72f298465219d636718a7140b72cf9f8e6909db7
Hyeonseob Yang <nick_yang_2@sfu.ca>
2021-02-08 00:06:09 -0800

    Add private fields to Client model and boilerplate methods

--- cbr_back_end/src/main/java/com/earth/cbr/models/Client.java
+++ cbr_back_end/src/main/java/com/earth/cbr/models/Client.java
@@ -1,6 +1,8 @@
 package com.earth.cbr.models;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Entity(name = "Client")
 @Table(name = "client")
@@ -13,28 +15,116 @@ public class Client {
             name = "name",
             columnDefinition = "TEXT"
     )
-    private String name;
+    private String firstName;
+    private String lastName;
+    private Integer birthDate;
 
-    public Client() {
-    }
+    @Transient
+    private Integer age;
 
-    public Client(String name) {
-        this.name = name;
+    private Character gender;
+    private String image;
+    private String zone;
+    private Integer villageNumber;
+    private Integer signupDate;
+    private String contactNumber;
+    private List<String> disabilities;
+    private Long cbrWorkerId;
+    private String caregiverContact;
+    private String requiredServices;
+    private String goals;
+
+    public Client(String firstName,
+                  String lastName,
+                  Integer birthDate,
+                  Character gender,
+                  String image,
+                  String zone,
+                  Integer villageNumber,
+                  Integer signupDate,
+                  String contactNumber,
+                  List<String> disabilities,
+                  Long cbrWorkerId,
+                  String caregiverContact,
+                  String requiredServices,
+                  String goals) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.image = image;
+        this.zone = zone;
+        this.villageNumber = villageNumber;
+        this.signupDate = signupDate;
+        this.contactNumber = contactNumber;
+        this.disabilities = disabilities;
+        this.cbrWorkerId = cbrWorkerId;
+        this.caregiverContact = caregiverContact;
+        this.requiredServices = requiredServices;
+        this.goals = goals;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getName() {
-        return name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Integer getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public Integer getVillageNumber() {
+        return villageNumber;
+    }
+
+    public Integer getSignupDate() {
+        return signupDate;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public List<String> getDisabilities() {
+        return disabilities;
+    }
+
+    public Long getCbrWorkerId() {
+        return cbrWorkerId;
+    }
+
+    public String getCaregiverContact() {
+        return caregiverContact;
+    }
+
+    public String getRequiredServices() {
+        return requiredServices;
+    }
+
+    public String getGoals() {
+        return goals;
     }
 }
