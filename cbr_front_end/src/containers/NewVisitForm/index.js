7ca67f6a54238150b3a4eaa15bc3a4d4ad248308
William Tjandra <williamtj2001@gmail.com>
2021-02-08 22:34:30 -0800

    Created the New Visit page and container

---
+++ cbr_front_end/src/containers/NewVisitForm/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import LoginInputField from "../../components/LoginInputField";
+import Logo from "../../assets/HHALogo.svg";
+import "./style.css";
+
+const NewVisitForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+
+}
+
+export default NewVisitForm;
---
+++ cbr_front_end/src/pages/NewVisit/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NewVisitForm from "../../containers/NewVisitForm"
+import "./style.css";
+
+const NewVisit = props => {
+    return (
+        <div clsasName="">
+            <NewVisitForm />
+        </div>
+    );
+}
+
+export default NewVisits;
