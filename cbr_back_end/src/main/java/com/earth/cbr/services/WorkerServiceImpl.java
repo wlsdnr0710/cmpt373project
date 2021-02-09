40b25e042468b545236e5239f486c5a5672c199f
nsteltin <noah_stelting@sfu.ca>
2021-02-08 22:01:50 -0800

    Add Worker Service Implementation class.
    Write the implementation details for the Worker Service. Overrides the methods of get, add, and delete.

---
+++ cbr_back_end/src/main/java/com/earth/cbr/services/WorkerServiceImpl.java
@@ -0,0 +1,49 @@
+package com.earth.cbr.services;
+
+import com.alibaba.fastjson.JSONObject;
+import com.earth.cbr.models.Worker;
+import com.earth.cbr.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WorkerServiceImpl implements WorkerService {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @Override
+    public List<Worker> getAllWorkers() {
+        return workerRepository.findAll();
+    }
+
+    @Override
+    public Worker getWorkerById(Long id) {
+        Optional<Worker> workerOptional = workerRepository.findById(id);
+        Worker worker = workerOptional.get();
+        return worker;
+    }
+
+    @Override
+    public Worker addWorker(JSONObject payload) {
+        String workerFirstName = (String) payload.get("first_name");
+        String workerLastName = (String) payload.get("last_name");
+        String workerUsername = (String) payload.get("username");
+        String workerPassword = (String) payload.get("password");
+        String workerPhone = (String) payload.get("phone");
+        String workerEmail = (String) payload.get("email");
+        String workerRole = (String) payload.get("role");
+        String workerZone = (String) payload.get("zone");
+        Worker worker = new Worker(workerFirstName, workerLastName, workerUsername, workerPassword, workerPhone,
+                workerEmail, workerRole, workerZone);
+        return workerRepository.save(worker);
+    }
+
+    @Override
+    public void deleteWorkerById(Long id) {
+        workerRepository.deleteById(id);
+    }
+}
