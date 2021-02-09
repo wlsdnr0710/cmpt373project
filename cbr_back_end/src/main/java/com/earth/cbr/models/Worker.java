007094b98a3f5022b13777515081d68d27d1ed14
nsteltin <noah_stelting@sfu.ca>
2021-02-08 21:09:40 -0800

    Add table columns to Worker class.
    Fill in the Worker class with all the required variables. Add the columns based on the names in the database workers table.

--- cbr_back_end/src/main/java/com/earth/cbr/models/Worker.java
+++ cbr_back_end/src/main/java/com/earth/cbr/models/Worker.java
@@ -9,7 +9,138 @@ public class Worker {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(
+            name = "first_name",
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+
+    @Column(
+            name = "username",
+            columnDefinition = "TEXT"
+    )
+    private String username;
+
+    @Column(
+            name = "password",
+            columnDefinition = "TEXT"
+    )
+    private String password;
+
+    @Column(
+            name = "phone",
+            columnDefinition = "TEXT"
+    )
+    private String phone;
+
+    @Column(
+            name = "email",
+            columnDefinition = "TEXT"
+    )
+    private String email;
+
+    @Column(
+            name = "role",
+            columnDefinition = "TEXT"
+    )
+    private String role;
+
+    @Column(
+            name = "zone",
+            columnDefinition = "TEXT"
+    )
+    private String zone;
+
     public Worker() {
 
+    }
+
+    public Worker(String firstName, String lastName, String username, String password, String phone, String email, String role, String zone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+        this.role = role;
+        this.zone = zone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
     }
 }
