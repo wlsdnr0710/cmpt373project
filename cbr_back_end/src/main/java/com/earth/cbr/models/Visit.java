71e4762929942f013ff243384cff6dc7f276c52b
nsteltin <noah_stelting@sfu.ca>
2021-02-09 19:06:03 -0800

    Add columns to Visit class.
    Create private variables for each column in the visit table.

--- cbr_back_end/src/main/java/com/earth/cbr/models/Visit.java
+++ cbr_back_end/src/main/java/com/earth/cbr/models/Visit.java
@@ -9,6 +9,54 @@ public class Visit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(
+            name = "consent",
+            columnDefinition = "BINARY"
+    )
+    private String consent;
+
+    @Column(
+            name = "date",
+            columnDefinition = "DATE"
+    )
+    private String date;
+
+    @Column(
+            name = "cbr_worker_name",
+            columnDefinition = "TEXT"
+    )
+    private String cbrWorkerName;
+
+    @Column(
+            name = "purpose",
+            columnDefinition = "TEXT"
+    )
+    private String purpose;
+
+    @Column(
+            name = "zone",
+            columnDefinition = "TEXT"
+    )
+    private String zone;
+
+    @Column(
+            name = "village_number",
+            columnDefinition = "INT"
+    )
+    private String villageNumber;
+
+    @Column(
+            name = "health_goal_progress",
+            columnDefinition = "TEXT"
+    )
+    private String healthGoalProgress;
+
+    @Column(
+            name = "health_outcome",
+            columnDefinition = "TEXT"
+    )
+    private String healthOutcome;
+
     public Visit() {
 
     }
