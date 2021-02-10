5c03577c7250309a32f9c77fc4c4ac9d465ebb04
nsteltin <noah_stelting@sfu.ca>
2021-02-09 20:14:49 -0800

    Create Visit Repository and Service.
    Create the Visit Repository interface which extends JpaRepository. Also made Visit Service interface with methods.

---
+++ cbr_back_end/src/main/java/com/earth/cbr/repositories/VisitRepository.java
@@ -0,0 +1,8 @@
+package com.earth.cbr.repositories;
+
+import com.earth.cbr.models.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+}
---
+++ cbr_back_end/src/main/java/com/earth/cbr/services/VisitService.java
@@ -0,0 +1,15 @@
+package com.earth.cbr.services;
+
+import com.alibaba.fastjson.JSONObject;
+import com.earth.cbr.models.Visit;
+
+import java.util.List;
+
+public interface VisitService {
+    List<Visit> getAllWorkers();
+    Visit getVisitByUsername(String username);
+    Visit getVisitById(Long id);
+    Visit addVisit(JSONObject payload);
+    Visit updateVisitById(Long id, JSONObject payload);
+    void deleteVisitById(Long id);
+}
