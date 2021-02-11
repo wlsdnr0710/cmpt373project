68aee7bae5fd9174242fa40efda381dde6f407cb
William Tjandra <williamtj2001@gmail.com>
2021-02-10 22:07:13 -0800

    Added checkHandlers, headers, styles

--- cbr_front_end/src/containers/NewVisitForm/index.js
+++ cbr_front_end/src/containers/NewVisitForm/index.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import FormHeader from "../../components/FormHeader";
 import axios from 'axios';
 import LoginInputField from "../../components/LoginInputField";
 import Logo from "../../assets/HHALogo.svg";
 import "./style.css";
 
-const NewVisitForm = () => {
-    return (
-        <div>
 
+const NewVisitForm = () => {
+    const isCBRCheckBoxActionHandler = event => {
+        const checkBox = event.target;
+        if (checkBox.checked) {
+            setIsFormInputDisabled(false);;
+        } else {
+            setIsFormInputDisabled(true);
+        }
+    };
+
+    return (
+        <div className="new-visit-form">
+            <FormHeader
+                headerText="New Visit - Visit Information"
+            />
+            <div className="form-body">
+
+                <hr />
+
+                <Button variant="primary" size="lg" disabled={isFormInputDisabled}>
+                    Submit
+                </Button>
+            </div>
         </div>
     );
 
