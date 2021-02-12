b9396914758c220bfd7204262938d40a32db62ee
Hyeonseob Yang <nick_yang_2@sfu.ca>
2021-02-11 23:18:26 -0800

    Change DefaultExceptionHandler to handle ConstraintViolationException

--- cbr_back_end/src/main/java/com/earth/cbr/exceptions/DefaultExceptionHandler.java
+++ cbr_back_end/src/main/java/com/earth/cbr/exceptions/DefaultExceptionHandler.java
@@ -4,32 +4,43 @@
 package com.earth.cbr.exceptions;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+
 
 @ControllerAdvice
 public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(
+            ConstraintViolationException ex, WebRequest request) {
+
+        JSONObject errorDetails = new JSONObject();
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(MissingRequiredKeyException.class)
     public ResponseEntity<Object> handleMissingRequiredKeyException(
-            MissingRequiredKeyException e, WebRequest request) {
+            MissingRequiredKeyException ex, WebRequest request) {
 
         JSONObject responseJson = new JSONObject();
-        responseJson.put("message", e.getMessage());
+        responseJson.put("message", ex.getMessage());
 
         return ResponseEntity.badRequest().body(responseJson);
     }
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<Object> handleInvalidDataException(
-            InvalidDataException e, WebRequest request) {
+            InvalidDataException ex, WebRequest request) {
 
         JSONObject responseJson = new JSONObject();
-        responseJson.put("message", e.getMessage());
+        responseJson.put("message", ex.getMessage());
 
         return ResponseEntity.badRequest().body(responseJson);
     }
