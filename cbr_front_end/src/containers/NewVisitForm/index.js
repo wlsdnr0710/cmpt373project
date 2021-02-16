d1c038663e32996a2a4828090479ea0e93c063f7
William Tjandra <williamtj2001@gmail.com>
2021-02-15 21:57:03 -0800

    Added all the required labels

--- cbr_front_end/src/containers/NewVisitForm/index.js
+++ cbr_front_end/src/containers/NewVisitForm/index.js
@@ -6,16 +6,34 @@ import CheckBox from "../../components/CheckBox";
 import axios from 'axios';
 import Logo from "../../assets/HHALogo.svg";
 import "./style.css";
-const defaultClientZones = {
+
+// TODO: We want to fetch zones from backend server instead of hardcoding them here.
+const defaultPurpose = {
     "CBR": "cbr",
     "Disability centre referral": "referral",
     "Disability centre follow up": "followUp",
+};
+
+
+const defaultClientZones = {
+    "BidiBidi Zone 1": "bidizone1",
+    "BidiBidi Zone 2": "bidizone2",
+    "BidiBidi Zone 3": "bidizone3",
+    "BidiBidi Zone 4": "bidizone4",
+    "BidiBidi Zone 5": "bidizone5",
+    "Palorinya Basecamp": "palBasecamp",
+    "Palorinya Zone 1": "palzone1",
+    "Palorinya Zone 2": "palzone2",
+    "Palorinya Zone 3": "palzone3",
 };
 
 const NewVisitForm = () => {
     const [formInputs, setFormInputs] = useState({
         "purposeForVisit": "cbr",
         "doHealthCheckBox": false,
+        "doEducationCheckBox": false,
+        "doSocialCheckBox": false,
+        "clientZone": "bidizone1",
 
     });
     const [isFormInputDisabled, setIsFormInputDisabled] = useState(false);
@@ -62,6 +80,18 @@ const NewVisitForm = () => {
         updateFormInputByNameValue("doHealthCheckBox", doHealthCheckBox);
     };
 
+    const doEducationCheckBoxActionHandler = event => {
+        const checkBox = event.target;
+        const doEducationCheckBox = checkBox.checked;
+        updateFormInputByNameValue("doEducationCheckBox", doEducationCheckBox);
+    };
+
+    const doSocialCheckBoxActionHandler = event => {
+        const checkBox = event.target;
+        const doSocialCheckBox = checkBox.checked;
+        updateFormInputByNameValue("doSocialCheckBox", doSocialCheckBox);
+    };
+
 
 
     return (
@@ -77,7 +107,7 @@ const NewVisitForm = () => {
                     <DropdownList
                         dropdownName="purposeForVisit"
                         value={formInputs["purposeForVisit"]}
-                        dropdownListItemsKeyValue={defaultClientZones}
+                        dropdownListItemsKeyValue={defaultPurpose}
                         onChange={formInputChangeHandler}
                         isDisabled={isFormInputDisabled}
                     />
@@ -88,9 +118,75 @@ const NewVisitForm = () => {
                         displayText={"Health"}
                         isHidden={false}
                     />
+                    <CheckBox
+                        name="doEducationCheckBox"
+                        value={formInputs["doEducationCheckBox"]}
+                        actionHandler={doEducationCheckBoxActionHandler}
+                        displayText={"Education"}
+                        isHidden={false}
+                    />
+                    <CheckBox
+                        name="doSocialCheckBox"
+                        value={formInputs["doSocialCheckBox"]}
+                        actionHandler={doSocialCheckBoxActionHandler}
+                        displayText={"Social"}
+                        isHidden={false}
+                    />
                 </div>
                 <hr />
+                <div>
+                    <label>Date of Visit:</label>
+                </div>
+                <div>
+                    <label>Name of CBR worker:</label>
+                </div>
+                <div>
+                    <label>Location of Visit(GPS)</label>
+                </div>
+                <div>
+                    <div>
+                        <label>Location</label>
+                    </div>
+                    <DropdownList
+                        dropdownName="clientZone"
+                        value={formInputs["clientZone"]}
+                        dropdownListItemsKeyValue={defaultClientZones}
+                        onChange={formInputChangeHandler}
+                        isDisabled={isFormInputDisabled}
+                    />
+                </div>
+                <div>
+                    <label>Village No:</label>
+                </div>
+                <div>
+                    <label>For Health what was provided?</label>
+                </div>
+                <div>
+                    <label>Goal met?:</label>
+                </div>
+                <div>
+                    <label>(if goal met = concluded) what was the outcome?</label>
+                </div>
+                <div>
+                    <label>For education what was provided?</label>
+                </div>
+                <div>
+                    <label>Goal met?:</label>
+                </div>
+                <div>
+                    <label>(if goal met = concluded) what was the outcome?</label>
+                </div>
+                <div>
+                    <label>For social what was provided?</label>
+                </div>
+                <div>
+                    <label>Goal met?:</label>
+                </div>
+                <div>
+                    <label>(if goal met = concluded) what was the outcome?</label>
+                </div>
 
+                <hr />
                 <Button
                     variant="primary"
                     size="lg"
