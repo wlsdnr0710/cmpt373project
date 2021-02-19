d663ebbcfc9f79e6e0bb7e428a803d9d7377c1f3
nsteltin <noah_stelting@sfu.ca>
2021-02-18 16:31:17 -0800

    Add constructor/getters/setters to the zone class.
    Create a constructor for zone and the corresponding getters and setters. This allows Spring Boot to get the columns from the database.

--- cbr_back_end/src/main/java/com/earth/cbr/models/Zone.java
+++ cbr_back_end/src/main/java/com/earth/cbr/models/Zone.java
@@ -16,4 +16,29 @@ public class Zone {
     )
     @NotBlank(message = "Name is mandatory")
     private String name;
+
+    public Zone() {
+
+    }
+
+    public Zone(Long id, @NotBlank(message = "Name is mandatory") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
