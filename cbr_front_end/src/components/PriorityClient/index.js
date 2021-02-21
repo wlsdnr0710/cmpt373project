23bc31327867597bfb31c215340811a97474a401
Yi Ching Chou <ycchou@sfu.ca>
2021-02-20 18:58:47 -0800

    Improve the priority client card layout
    Improve the styles by team members' feedback.

--- cbr_front_end/src/components/PriorityClient/index.js
+++ cbr_front_end/src/components/PriorityClient/index.js
@@ -1,35 +1,40 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import avatar from "../../assets/avatar.png";
 import "./style.css";
 
 const PriorityClient = (props) => {
     // TODO: Process Risk level to Critical/High/Medium/Low if needed. 
+    const history = useHistory();
+
+    // TODO: make the card clickable when we actually get client id from API
+    const onClickCardHandler = event => {
+        history.push("/client-information?id=" + props.client.id);
+        window.scrollTo(0, 0);
+    };
+    
     return (
         <div className='priority-client'>
-            <Card style={{ width: '100%', maxWidth: '500px' }}>
-                <Card.Img variant="top" src={avatar} />
+            <Card style={{ width: '100%', maxWidth: '350px' }}>
+                <div className="client-photo-container">
+                    <img src={avatar} alt="" style={{width: "100%"}} />
+                </div>
                 <Card.Body>
-                    <Card.Title>{props.client.Name}   <Badge variant="danger">Danger</Badge></Card.Title>
-                    <div className="attribute">
-                        <div className="attribute-title">Risk</div>
-                        <div className="attribute-value">{props.client.Risk}</div>
+                    <div className="client-name">{props.client.Name}</div>
+                    <div>
+                        <Badge variant="danger">{props.client.Risk}</Badge>
                     </div>
-
+   
                     <div className="attribute">
                         <div className="attribute-title">Zone</div>
                         <div className="attribute-value">{props.client.Zone}</div>
-                    </div>
-                    <div className="button-container">
-                        <Button variant="primary">View</Button>
                     </div>
                 </Card.Body>
             </Card>
         </div>
     );
 };
-
 
 export default PriorityClient;
