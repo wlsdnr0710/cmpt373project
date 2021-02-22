c23f768da01fa2a7d41505e0b1fd2ca87485d081
damarasi <damarasi@sfu.ca>
2021-02-22 03:14:23 -0800

    rename healthoption.java

---
+++ cbr_back_end/src/main/java/com/earth/cbr/models/ServiceOption.java
@@ -0,0 +1,59 @@
+package com.earth.cbr.models;
+
+import javax.persistence.*;
+
+@Entity(name = "Health_Option")
+@Table(name = "health_option")
+public class ServiceOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+        name = "health_option",
+        columnDefinition = "TEXT" 
+    )
+    private String healthOption;
+
+   
+    @Column(
+        name = "type",
+        columnDefinition = "TEXT"
+    )
+    private String type;
+    
+    public ServiceOption(){
+        
+    }
+
+    public ServiceOption(String healthOption, String type) {
+        this.healthOption = healthOption;
+        this.type = type;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getHealthOption(){
+        return healthOption;
+    }
+
+    public void setHealthOption(String healthOption){
+        this.healthOption = healthOption;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    
+}
