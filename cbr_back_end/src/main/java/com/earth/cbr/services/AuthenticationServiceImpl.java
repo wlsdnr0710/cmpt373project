b21829684dd44cd549d47abcd8cff532e58f8220
Yi Ching Chou <ycchou@sfu.ca>
2021-02-25 15:06:25 -0800

    Implement authentication service
    Implement authentication service to check username and password for the login credential.

--- cbr_back_end/src/main/java/com/earth/cbr/services/AuthenticationServiceImpl.java
+++ cbr_back_end/src/main/java/com/earth/cbr/services/AuthenticationServiceImpl.java
@@ -1,17 +1,36 @@
 package com.earth.cbr.services;
 
+import com.earth.cbr.models.Worker;
 import com.earth.cbr.models.authentication.Credential;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Autowired
+    private WorkerService workerService;
+
     @Override
     public boolean isCredentialValid(Credential credential) {
-        return true;
+        return areUsernamePasswordValid(credential.username, credential.password);
     }
 
     @Override
     public String getAuthenticationToken(Credential credential) {
         return "JWT token";
+    }
+
+    private boolean areUsernamePasswordValid(String username, String password) {
+        if (!doesUsernameExist(username)) {
+            return false;
+        }
+        Worker worker = workerService.getWorkerByUsername(username);
+        return worker.getPassword().equals(password) ? true : false;
+    }
+
+    private boolean doesUsernameExist(String username) {
+        Worker worker = workerService.getWorkerByUsername(username);
+        return worker != null ? true : false;
     }
 }
