18aa25c7722afcfdb5ef4c7b33e3957c6c9bc253
nsteltin <noah_stelting@sfu.ca>
2021-03-01 19:38:43 -0800

    Add GET by id and POST add to zone controller.
    Add GET mapping to get a specific zone by it's unique id. Also add POST mapping to add a new zone to the database.

--- cbr_back_end/src/main/java/com/earth/cbr/controllers/ZoneController.java
+++ cbr_back_end/src/main/java/com/earth/cbr/controllers/ZoneController.java
@@ -1,14 +1,14 @@
 package com.earth.cbr.controllers;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.earth.cbr.exceptions.MissingRequiredDataObjectException;
+import com.earth.cbr.models.Message;
 import com.earth.cbr.models.Zone;
 import com.earth.cbr.services.ZoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +24,35 @@ public class ZoneController {
         List<Zone> zones = zoneService.getAllZones();
         JSONObject responseJson = new JSONObject();
         responseJson.put("data", zones);
+        return ResponseEntity.ok().body(responseJson);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<JSONObject> getZoneById(@PathVariable Long id){
+        Zone zone = zoneService.getZoneById(id);
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("data", zone);
+        return ResponseEntity.ok().body(responseJson);
+    }
+
+    @PostMapping
+    public ResponseEntity<JSONObject> addZone(@RequestBody JSONObject payload)
+            throws MissingRequiredDataObjectException {
+        JSONObject messageJSON = payload.getJSONObject("data");
+
+        if (messageJSON == null) {
+            throw new MissingRequiredDataObjectException("Missing data object containing Zone data");
+        }
+        String zoneString = messageJSON.toJSONString();
+
+        JSONObject responseJson = new JSONObject();
+        Zone zone = JSON.parseObject(zoneString, Zone.class);
+
+        Zone addedZone = zoneService.addZone(zone);
+
+        // Need to tell front-end the new message's id
+        // so front-end can update the UI
+        responseJson.put("id", addedZone.getId());
         return ResponseEntity.ok().body(responseJson);
     }
 }
