00d3be23629fa0d15002ad63ca65d01f382393d0
Yi Ching Chou <ycchou@sfu.ca>
2021-03-05 12:58:04 -0800

    User can enable or disable CameraSnapshot
    User can enable or disable CameraSnapshot to turn on or turn off the camera. Also improve the styles.

--- cbr_front_end/src/containers/CameraSnapshot/index.js
+++ cbr_front_end/src/containers/CameraSnapshot/index.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
+import CameraSVG from "../../assets/svg/camera.svg";
 import "./style.css";
 
 const CameraSnapshot = props => {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const [stream, setStream] = useState(null);
+    const [isPhotoTaken, setIsPhotoTaken] = useState(false);
+    const [isCameraSnapshotEnabled, setIsCameraSnapshotEnabled] = useState(false);
     const [facingMode, setFacingMode] = useState("user");
 
     useEffect(() => {
-        if (!doesSupportMediaDevice()) {
+        if (!doesSupportMediaDevice() || !isCameraSnapshotEnabled) {
             return;
         }
 
@@ -26,10 +29,11 @@ const CameraSnapshot = props => {
                 const track = tracks[index];
                 track.stop();
             }
+            setStream(null);
         };
 
         return turnOffCamera;
-    }, [stream]);
+    }, [stream, isCameraSnapshotEnabled]);
 
     const doesSupportMediaDevice = () => {
         return 'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices;
@@ -51,24 +55,69 @@ const CameraSnapshot = props => {
         });
     };
 
+    const getCameraSnapshotPlaceholder = () => {
+        return (
+            <div className="camera-snapshot-placeholder">
+                <img className="camera-icon" src={CameraSVG} alt="camera-icon" />
+                <button className="primary-button" onClick={onClickEnableCamera}>Enable Camera</button>
+            </div>
+        );
+    };
+
+    const onClickEnableCamera = () => {
+        setIsCameraSnapshotEnabled(true);
+    };
+
+    const getCameraSnapshotUI = () => {
+        return (
+            <div>
+                <div style={{display: isPhotoTaken ? "none" : "block"}}>
+                    <video ref={videoRef} width="320" height="240" >
+                    </video>
+                </div>
+                <div style={{display: isPhotoTaken ? "block" : "none"}}>
+                    <canvas ref={canvasRef} width="320" height="240" >
+                    </canvas>
+                </div>
+                <div className="button-action">
+                    {getActionButton()}
+                </div>
+            </div>
+        );
+    };
+
+    const getActionButton = () => {
+        if (isPhotoTaken) {
+            return (
+                <button className="secondary-button" onClick={onClickReset}>Reset</button>
+            );
+        } else {
+            return (
+                <div>
+                    <button className="primary-button" onClick={onClickTakePhoto}>Take Photo</button>
+                    <button className="secondary-button" onClick={onClickDisableCamera}>Disable Camera</button>
+                </div>
+            );
+        }
+    };
+
+    const onClickReset = () => {
+        setIsPhotoTaken(false);
+    };
+
     const onClickTakePhoto = () => {
+        setIsPhotoTaken(true);
         const context = canvasRef.current.getContext('2d');
         context.drawImage(videoRef.current, 0, 0, 320, 240);
     };
 
+    const onClickDisableCamera = () => {
+        setIsCameraSnapshotEnabled(false);
+    };
+
     return (
         <div className="camera-snapshot">
-            <div>
-                <video ref={videoRef} width="320" height="240" >
-                </video>
-            </div>
-            <div>
-                <canvas ref={canvasRef} width="320" height="240" >
-                </canvas>
-            </div>
-            <div>
-                <button onClick={onClickTakePhoto}>Take Photo</button>
-            </div>
+            {isCameraSnapshotEnabled ? getCameraSnapshotUI() : getCameraSnapshotPlaceholder()}
         </div>
     );
 };
