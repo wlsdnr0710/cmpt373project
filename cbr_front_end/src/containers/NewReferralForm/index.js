7d838dc78107e7fda11b662d6d8f3bba17bf1810
Yi Ching Chou <ycchou@sfu.ca>
2021-03-07 19:19:11 -0800

    Show text area when users select other required service
    Show text area when users select other required service for description.

--- cbr_front_end/src/containers/NewReferralForm/index.js
+++ cbr_front_end/src/containers/NewReferralForm/index.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import FormHeader from "../../components/FormHeader";
 import RequiredServiceCheckBoxes from "../../components/RequiredServiceCheckBoxes";
+import TextAreaInputField from "../../components/TextAreaInputField";
 import "./style.css";
 
 const NewReferralForm = props => {
     const [formInputs, setFormInputs] = useState({
         "requiredServices": [],
+        "requiredServiceOtherDescription": "",
     });
+    const [showOtherDescription, setShowOtherDescription] = useState(false);
 
     const requiredServicesKeyValues = {
         "physiotherapy": "1",
         "orthotic": "2",
         "wheelchair": "3",
-        "other ": "4",
+        "other": "4",
     };
 
     const getRequiredServicesCheckBoxesOnChangeHandler = name => {
@@ -25,7 +28,17 @@ const NewReferralForm = props => {
                 removeCheckBoxValuesByName(checkBoxesValues, name);
             }
             updateFormInputByNameValue("requiredServices", checkBoxesValues);
+
+            if (isRequiredServiceOtherChecked(checkBoxesValues)) {
+                setShowOtherDescription(true);
+            } else {
+                setShowOtherDescription(false);
+            }
         };
+    };
+
+    const isRequiredServiceOtherChecked = checkBoxesValues => {
+        return checkBoxesValues.indexOf(requiredServicesKeyValues["other"]) !== -1;
     };
 
     const updateFormInputByNameValue = (name, value) => {
@@ -43,6 +56,32 @@ const NewReferralForm = props => {
         }
     };
 
+    const showOtherDescriptionTextarea = () => {
+        if (!showOtherDescription) {
+            return null;
+        }
+
+        return (
+            <div>
+                <div>Please describe:</div>
+                <TextAreaInputField 
+                    name={"requiredServiceOtherDescription"} 
+                    value={formInputs["requiredServiceOtherDescription"]} 
+                    onChange={formInputChangeHandler} 
+                    rows="4" 
+                    isDisabled={false}
+                />
+            </div>
+        );
+    };
+
+    const formInputChangeHandler = event => {
+        const input = event.target;
+        const name = input.name;
+        const value = input.value;
+        updateFormInputByNameValue(name, value);
+    };
+
     return (
         <div className="new-referral-form">
             <FormHeader
@@ -55,6 +94,7 @@ const NewReferralForm = props => {
                     getOnChangeHandlers={getRequiredServicesCheckBoxesOnChangeHandler}
                     isDisabled={false}
                 />
+                {showOtherDescriptionTextarea()}
             </div>
         </div>
     );
