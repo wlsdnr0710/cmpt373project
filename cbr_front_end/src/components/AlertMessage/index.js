5480dbff3fdacc279d66777ba84c44a4d654835a
nsteltin <noah_stelting@sfu.ca>
2021-03-08 18:03:17 -0800

    Connect message board to back-end.
    Populate the alert messages in the dashboard with the data from the back-end. Format the alerts to have different colors based on their priority. List the date and the message.

--- cbr_front_end/src/components/AlertMessage/index.js
+++ cbr_front_end/src/components/AlertMessage/index.js
@@ -3,10 +3,12 @@ import Alert from 'react-bootstrap/Alert';
 import "./style.css";
 
 //TODO: Add props priority that we can use to sort Alert Message
-const AlertMessage = ({message, variant}) => {
+const AlertMessage = ({message, date, variant}) => {
     return (
         <div className="alert-message">
-            <Alert variant={variant ? variant : "primary"}>{message}</Alert >
+            <Alert variant={variant ? variant : "primary"}>
+                {date} - {message}
+            </Alert >
         </div>
     );
 };
--- cbr_front_end/src/containers/AlertMessageBoard/index.js
+++ cbr_front_end/src/containers/AlertMessageBoard/index.js
@@ -1,27 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { getToken } from "../../utils/AuthenticationUtil";
+import { parseDateStringToEpoch, parseEpochToDateString } from "../../utils/Utilities";
+import axios from 'axios';
 import AlertMessage from '../../components/AlertMessage';
+import ServerConfig from "../../config/ServerConfig";
 import Button from 'react-bootstrap/Button';
 import './style.css';
 
 const AlertMessageBoard = () => {
     const [alertMessages, setAlertMessages] = useState({});
 
+    const variants = ["primary", "warning", "danger"];
+
     const getAlertMessage = () => {
         // TODO: get alert message from backend.
-        setAlertMessages([
-            {
-                "variant": "primary",
-                "message": "Welcome to the CBR worker management system.",
-            },
-            {
-                "variant": "danger",
-                "message": "A client has high risk level!",
-            },
-            {
-                "variant": "warning",
-                "message": "Please complete your profile.",
-            },
-        ]);
+        const requestHeader = {
+                    token: getToken()
+                };
+
+        axios.get(
+                ServerConfig.api.url + "/api/v1/message",
+                {
+                    headers: requestHeader,
+                }
+            )
+            .then(response => {
+            var list = new Array();
+            for(var i = 0; i < response.data.data.length; i++) {
+                const eachPriority = variants[response.data.data[i].priority-1]
+                list[i] = {
+                        "variant": eachPriority,
+                        "message": response.data.data[i].message,
+                        "date": response.data.data[i].date,
+                };
+            }
+
+            console.log(list);
+                setAlertMessages(list);
+            });
     };
 
     useEffect(() => {
@@ -37,7 +53,8 @@ const AlertMessageBoard = () => {
             for (const index in alertMessages) {
                 const message = alertMessages[index].message;
                 const variant = alertMessages[index].variant;
-                alertMessageComponents.push(<AlertMessage message={message} variant={variant} key={index}/>)
+                const date = alertMessages[index].date;
+                alertMessageComponents.push(<AlertMessage message={message} variant={variant} date={date}/>)
             }
             return alertMessageComponents;
         }
