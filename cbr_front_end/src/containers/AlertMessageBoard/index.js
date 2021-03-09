ab829b52f66e7421be12fe6cf578565e1ec92021
nsteltin <noah_stelting@sfu.ca>
2021-03-08 22:15:16 -0800

    Create Add button and text box entry.
    Make Add button that allows the worker to add a new message. Use a POST request to send the data to the back-end.

--- cbr_front_end/src/containers/AlertMessageBoard/index.js
+++ cbr_front_end/src/containers/AlertMessageBoard/index.js
@@ -4,6 +4,7 @@ import { parseDateStringToEpoch, parseEpochToDateString } from "../../utils/Util
 import axios from 'axios';
 import AlertMessage from '../../components/AlertMessage';
 import ServerConfig from "../../config/ServerConfig";
+import TextAreaInputField from "../../components/TextAreaInputField";
 import Button from 'react-bootstrap/Button';
 import './style.css';
 
@@ -11,12 +12,12 @@ const AlertMessageBoard = () => {
     const [alertMessages, setAlertMessages] = useState({});
 
     const variants = ["primary", "warning", "danger"];
+    let messageTextValue;
 
-    const getAlertMessage = () => {
-        // TODO: get alert message from backend.
+    const getAllAlertMessages = () => {
         const requestHeader = {
-                    token: getToken()
-                };
+            token: getToken()
+        };
 
         axios.get(
                 ServerConfig.api.url + "/api/v1/message",
@@ -29,22 +30,44 @@ const AlertMessageBoard = () => {
             for(var i = 0; i < response.data.data.length; i++) {
                 const eachPriority = variants[response.data.data[i].priority-1]
                 list[i] = {
-                        "variant": eachPriority,
-                        "message": response.data.data[i].message,
-                        "date": response.data.data[i].date,
+                    "variant": eachPriority,
+                    "message": response.data.data[i].message,
+                    "date": parseEpochToDateString(response.data.data[i].date),
                 };
             }
-
-            console.log(list);
-                setAlertMessages(list);
+            setAlertMessages(list);
             });
     };
 
+    const addNewAlertMessage = () => {
+        const requestHeader = {
+            token: getToken()
+        };
+
+        const data = { "workerId": 1, "message": "All staff pizza lunch", "priority": 2, "date": "2020-03-05"
+        }
+
+        axios.post(ServerConfig.api.url + '/api/v1/message', {
+            "data": data
+        }, {
+            headers: requestHeader,
+        })
+        .catch(error => {
+                       console.log(error.response.data);
+                    })
+    }
+
+
+    const parseISODateString = ISODateString => {
+        const epoch = parseDateStringToEpoch(ISODateString);
+        return parseEpochToDateString(epoch);
+    };
+
     useEffect(() => {
-        getAlertMessage();
+        getAllAlertMessages();
     }, []);
 
-    const createMessageCompoments = () => {
+    const createMessageComponents = () => {
         const alertMessageComponents = [];
         if (!alertMessages) {
             return (<AlertMessage message={"There is no alert now"} />)
@@ -64,9 +87,19 @@ const AlertMessageBoard = () => {
         <div className="alert-board">
             <div className="alert-board-title">Alert Messages</div> 
             <div className="messages">
-                {createMessageCompoments()}
+                {createMessageComponents()}
             </div>
-            <Button onClick={getAlertMessage} variant="primary">Refresh</Button>
+            <Button onClick={getAllAlertMessages} variant="primary">Refresh</Button>
+            &nbsp;
+            <Button variant="primary">Add</Button>
+            <div className="add">
+            <TextAreaInputField
+                name="message"
+                value={messageTextValue}
+                disabled={false}
+            />
+            <Button onClick={addNewAlertMessage} variant="primary">Submit</Button>
+            </div>
         </div>
     );
 };
