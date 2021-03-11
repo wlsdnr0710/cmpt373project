10ac64d8cc018c326f01566f5e7de86f769a7e36
jinwookk <jinwook_kim@sfu.ca>
2021-03-11 09:09:36 -0800

    Added Home and removed unnecessary entries.

--- cbr_front_end/src/containers/MobileNavigationBar/index.js
+++ cbr_front_end/src/containers/MobileNavigationBar/index.js
@@ -7,16 +7,10 @@ import newClientIcon from "../../assets/svg/navigation_icons/user_plus.svg";
 import newVisitIcon from "../../assets/svg/navigation_icons/user_pin.svg";
 import newReferralIcon from "../../assets/svg/navigation_icons/id_card.svg";
 import allClientsIcon from "../../assets/svg/navigation_icons/layers_alt.svg";
-import cloudSyncIcon from "../../assets/svg/navigation_icons/cloud.svg";
-import logoutIcon from "../../assets/svg/navigation_icons/logout.svg";
-import hamburgerMenuIcon from "../../assets/svg/navigation_icons/hamburger.svg";
-import logo from "../../assets/HHALogo.svg";
+import homeIcon from "../../assets/svg/navigation_icons/home.svg";
 import "./style.css";
 
-//TODO: Find way to import all assets at once 
 const PageTemplate = ({ children }) => {
-
-
   const getNavigationItems = () => {
     return (
       <div className="mobile-navigation-bar">
@@ -28,25 +22,11 @@ const PageTemplate = ({ children }) => {
           iconAlt="Dashboard"
         />
         <MobileNavigationBarEntry
-          label="New Client"
-          destination="/new-client"
+          label="Home Icon"
+          destination="/home"
           query="#"
-          iconSource={newClientIcon}
-          iconAlt="New Client"
-        />
-        <MobileNavigationBarEntry
-          label="New Visit"
-          destination="/view-client"
-          query="visits"
-          iconSource={newVisitIcon}
-          iconAlt="New Visit"
-        />
-        <MobileNavigationBarEntry
-          label="New Referral"
-          destination="#"
-          query="#"
-          iconSource={newReferralIcon}
-          iconAlt="New Referral"
+          iconSource={homeIcon}
+          iconAlt="Home"
         />
         <MobileNavigationBarEntry
           label="All Clients"
@@ -56,22 +36,6 @@ const PageTemplate = ({ children }) => {
           iconAlt="All Clients"
         />
       </div>
-    );
-  };
-
-  const getSideBarMobileOnState = (showSideBarMobile) => {
-    if (showSideBarMobile) {
-      return (
-        <div className="side-navigation-mobile">{getNavigationItems()}</div>
-      );
-    } else {
-      return;
-    }
-  };
-
-  const getSideBarDesktop = () => {
-    return (
-      <div className="side-navigation-desktop">{getNavigationItems()}</div>
     );
   };
 
