db2b36d8f24bc2cf3e0c80be351ce5f165af2fb5
jinwookk <jinwook_kim@sfu.ca>
2021-03-12 18:29:29 -0800

    Added query validity checking function

--- cbr_front_end/src/components/NavigationBarEntry/index.js
+++ cbr_front_end/src/components/NavigationBarEntry/index.js
@@ -9,12 +9,16 @@ const displayIcon = (iconSource, iconAlt) => {
   }
 };
 
+const isQueryValid = query => {
+  return query !== "#" && query !== "";
+}
+
 //TODO: change if condition for destination in the future to reduce dependency
 const NavigationBarEntry = ({ label, destination, iconSource, iconAlt, query }) => {
   let history = useHistory();
   const OnClickNavigationHandler = () => {
     if (destination !== "#") {
-      if (query) {
+      if (isQueryValid(query)) {
         history.push(destination + "?query=" + query);
       } else {
         history.push(destination)
