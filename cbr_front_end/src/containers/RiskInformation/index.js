605104720a5f155890f2c9fa427ccce25c6fa436
Adrian <adrianwong.wt@gmail.com>
2021-03-14 01:13:54 -0800

    Fix save conflict

--- cbr_front_end/src/containers/RiskInformation/index.js
+++ cbr_front_end/src/containers/RiskInformation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import RiskUpdateEntry from "../../components/RiskUpdateEntry";
 import "./style.css";
 
@@ -10,6 +10,22 @@ const emptyRiskUpdate = () => {
   );
 };
 
+const getRiskUpdateList = (separatedRiskUpdates) => {
+  const riskHistoriesListIsEmpty =
+    separatedRiskUpdates.mostRecentUpdate === undefined ||
+    separatedRiskUpdates.pastHistory.length < 1;
+
+  if (riskHistoriesListIsEmpty) {
+    return emptyRiskUpdate();
+  } else {
+    return (
+      <RiskUpdateEntry
+        key={separatedRiskUpdates.mostRecentUpdate.id}
+        riskObject={separatedRiskUpdates.mostRecentUpdate}
+      />
+    );
+  }
+};
 
 const getPastRiskUpdateList = (riskHistories) => {
   console.log(riskHistories);
@@ -23,9 +39,46 @@ const getPastRiskUpdateList = (riskHistories) => {
   }
 };
 
+//spliceRiskHistory getting past a riskHistory w/ length of 0
+const spliceRiskHistory = (riskHistories) => {
+  let mostRecentUpdate = riskHistories[0];
+  let mostRecentUpdateIndex = 0;
+  for (let i = 0; i < riskHistories.length; i++) {
+    if (riskHistories[i].id < mostRecentUpdate.id) {
+      mostRecentUpdate = riskHistories[i];
+      mostRecentUpdateIndex = i;
+    }
+  }
+  return {
+    mostRecentUpdate: mostRecentUpdate,
+    pastHistory: riskHistories.splice(mostRecentUpdateIndex),
+  };
+};
+
 const RiskInformation = ({ riskHistories }) => {
+  const [showMoreRiskUpdates, setShowMoreRiskUpdates] = useState(false);
+  const separatedRiskUpdates = spliceRiskHistory(riskHistories);
+  console.log(separatedRiskUpdates);
+
+  const toggleShowMoreRiskUpdates = () => {
+    setShowMoreRiskUpdates(!showMoreRiskUpdates);
+  };
+
+  const showMoreRiskUpdatesOnState = (showMoreRiskUpdates) => {
+    if (showMoreRiskUpdates) {
+      getPastRiskUpdateList(separatedRiskUpdates.pastHistory);
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div>
+      {/* <button onClick={toggleShowMoreRiskUpdates}> */}
+      <button>
+        Show More
+      </button>
+      {/* {showMoreRiskUpdatesOnState(showMoreRiskUpdates)} */}
       {getPastRiskUpdateList(riskHistories)}
     </div>
   );
