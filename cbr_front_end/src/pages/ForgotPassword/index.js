32a30bd11c394095a41c02669dc5683b333c9405
William Tjandra <wntjandr@sfu.ca>
2021-03-23 23:49:24 -0700

    Removed unnecessary spacing

--- cbr_front_end/src/pages/ForgotPassword/index.js
+++ cbr_front_end/src/pages/ForgotPassword/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { doAuthentication } from "../../utils/AuthenticationUtil";
 import ForgotPasswordForm from "../../containers/ForgotPasswordForm"
 import "./style.css";
+
 const ForgotPassword = ({ history }) => {
     doAuthentication(history);
     return (
