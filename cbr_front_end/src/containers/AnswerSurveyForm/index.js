d0fb3c54fa804b15c2ce54ebcbe2889518e50e41
Yi Ching Chou <ycchou@sfu.ca>
2021-03-27 15:37:04 -0700

    Add select survey dropdown for answer survey page
    Users can select a survey from the dropdown to answer.

--- cbr_front_end/src/containers/AnswerSurveyForm/index.js
+++ cbr_front_end/src/containers/AnswerSurveyForm/index.js
@@ -6,6 +6,7 @@ import "./style.css";
 
 const AnswerSurveyForm = () => {
     const [surveys, setSurveys] = useState([]);
+    const [selectedSurvey, setSelectedSurvey] = useState("");
 
     useEffect(() => {
         const getSurveyList = () => {
@@ -16,15 +17,50 @@ const AnswerSurveyForm = () => {
                 .then(response => {
                     const responseSurveys = response.data.data;
                     setSurveys(responseSurveys);
+                    setDefaultSelectedSurvey(responseSurveys);
                 });
+        };
+
+        const setDefaultSelectedSurvey = surveys => {
+            const keys = Object.keys(surveys);
+            const firstSurveyKey = keys[0];
+            const firstSurveyId = surveys[firstSurveyKey]["id"];
+            setSelectedSurvey(firstSurveyId);
         };
 
         getSurveyList();
     }, []);
 
+    const getSurveyIdsNamesForDropdownList = () => {
+        const surveyNameIdMap = {};
+        for (let i = 0; i < surveys.length; i++) {
+            const survey = surveys[i];
+            const id = survey["id"];
+            const name = "[ID=" + id + "] " + survey["name"];
+            surveyNameIdMap[name] = id;
+        }
+        return surveyNameIdMap;
+    };
+
+    const onChangeSurveyDropdown = event => {
+        const dropdown = event.target;
+        const value = dropdown.value;
+        setSelectedSurvey(value);
+    };
+
     return (
         <div className="answer-survey-form">
-
+            <div className="select-survey-dropdown">
+                <div>
+                    Select a survey:
+                </div>
+                <DropdownList
+                    dropdownName="selected-survey"
+                    dropdownListItemsKeyValue={getSurveyIdsNamesForDropdownList()}
+                    value={selectedSurvey}
+                    onChange={onChangeSurveyDropdown}
+                />
+            </div>
         </div>
     );
 };
