a3fe24cc2e0d027431fad4d9480002c52e95948d
nsteltin <noah_stelting@sfu.ca>
2021-03-27 16:00:32 -0700

    Connect outstanding referral list to API.
    Add an axios call that retrieves all outstanding referrals sorted by date. Put them in a list and send to the component

--- cbr_front_end/src/containers/OutstandingReferralsList/index.js
+++ cbr_front_end/src/containers/OutstandingReferralsList/index.js
@@ -6,11 +6,44 @@ import OutstandingReferral from "../../components/OutstandingReferral";
 import "./style.css";
 
 const OutstandingReferralsList = () => {
+    const [outstandingReferrals, setOutstandingReferrals] = useState([]);
+
+    const getOutstandingReferrals = () => {
+        const requestHeader = {
+            token: getToken()
+        };
+        axios.get(
+            ServerConfig.api.url + "/api/v1/client/outstandingReferrals",
+            {
+                headers: requestHeader,
+            }
+        )
+        .then(response => {
+            setOutstandingReferrals(response.data.data);
+        });
+    };
+
+    useEffect(()=> {
+        getOutstandingReferrals();
+    }, []);
+
+    const createOutstandingReferralsListComponents = () => {
+        const outstandingReferralsComponents = [];
+        if(outstandingReferrals === undefined || outstandingReferrals.length === 0) {
+            return (<p>Currently there are no outstandingReferrals.</p>);
+        }
+        else {
+            for (const index in outstandingReferrals) {
+                outstandingReferralsComponents.push(<outstandingReferrals number={index} client={outstandingReferrals[index]} key={index}/>);
+            }
+            return outstandingReferralsComponents;
+        }
+    };
 
     return (
         <div className='outstanding-referrals-list'>
             <div className="outstanding-referrals-title">Outstanding Referrals</div>
-            <OutstandingReferral/>
+            {createOutstandingReferralsListComponents()}
         </div>
     );
 }
