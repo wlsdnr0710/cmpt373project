777873ec8640cd06f0dca449984e3e53c87e01dc
Adrian <adrianwong.wt@gmail.com>
2021-03-30 20:42:30 -0700

    Add runtime cache for images, requests are still WIP

--- cbr_front_end/workbox-config.js
+++ cbr_front_end/workbox-config.js
@@ -1,7 +1,26 @@
 module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{json,ico,html,png,txt,js,css,svg}"
-  ],
-  "swDest": "build/sw.js"
+    globDirectory: "build/",
+    // Continue to cache svg since all icons are svg 
+    globPatterns: ["**/*.{html,json,js,css,svg}"],
+    swDest: "build/sw.js",
+    // Cache images at runtime rather in preCache to avoid caching large amounts of large image files
+    runtimeCaching: [
+        {
+            urlPattern: /\.(?:png|jpg|jpeg)$/,
+            handler: "CacheFirst",
+            options: {
+                cacheName: "images",
+            },
+        },
+        {
+            urlPattern: ({url}) => url.pathname.startsWith("http://localhost:8080/api/v1/"),
+            handler: "StaleWhileRevalidate",
+            options: {
+                cacheName: "requests",
+            }
+        }
+    ],
 };
+//Might need to change swDest since sw.js must be in public
+//TODO: remove the use of workbox-config and the generateSW from package.json and instead use library to create own SW
+// or configure workbox-config to cache requests
