fc78afd297f0362a93fe583d9fa3ebbe8bf62ed5
Adrian <adrianwong.wt@gmail.com>
2021-03-31 23:55:04 -0700

    Add page caching
    Will cache requests and serve them when there is an issue with the network

--- cbr_front_end/workbox-config.js
+++ cbr_front_end/workbox-config.js
@@ -1,6 +1,12 @@
+// TODO: Get  
+// import {getServerConfigByEnvironment} from "./src/config/ServerConfig";
+// const server = require("./config/ServerConfig.js")
+// const server = getServerConfigByEnvironment();
+
+
 module.exports = {
     globDirectory: "build/",
-    // Continue to cache svg since all icons are svg 
+    // Continue to cache svg since all icons are svg
     globPatterns: ["**/*.{html,json,js,css,svg}"],
     swDest: "build/sw.js",
     // Cache images at runtime rather in preCache to avoid caching large amounts of large image files
@@ -13,14 +19,27 @@ module.exports = {
             },
         },
         {
-            urlPattern: ({url}) => url.pathname.startsWith("http://localhost:8080/api/v1/"),
+            urlPattern: new RegExp("http://localhost:8080/api/v1/"),
             handler: "StaleWhileRevalidate",
             options: {
                 cacheName: "requests",
-            }
-        }
+                expiration: {
+                    maxEntries: 50,
+                    // Set max age of entries to 2 days 
+                    maxAgeSeconds: 48 * 60 * 60,
+                },
+                cacheableResponse: { statuses: [200] },
+            },
+        },
+        // {
+        //     urlPattern: new RegExp("http://localhost:8080/api/v1/"),
+        //     handler: "CacheFirst",
+        //     options: {
+        //         cacheName: "failed"
+        //     },
+        //     cacheableResponse: { statuses: [] }, <-- problem, there is no error code returned if disconnected 
+        // }
     ],
 };
-//Might need to change swDest since sw.js must be in public
 //TODO: remove the use of workbox-config and the generateSW from package.json and instead use library to create own SW
 // or configure workbox-config to cache requests
