6a54984f5c6e18cdc71534fff01e159cd7100cd8
Adrian <adrianwong.wt@gmail.com>
2021-04-04 23:59:33 -0700

    Bug fix
    Fixed workbox not caching third party sites

--- cbr_front_end/public/generator.js
+++
@@ -1,8 +0,0 @@
-const workbox = require("workbox-build");
-
-workbox.generateSW({
-    cacheId:"cbr_worker",
-    globDirectory: "./",
-    globPatterns: ["**/*.{html,json,js,css,svg}"],
-    swDest: "./service-worker.js",
-})
--- cbr_front_end/src/serviceWorker.js
+++ cbr_front_end/src/serviceWorker.js
@@ -8,12 +8,10 @@ importScripts(
 
 const MAX_RETRY_MIN = 3 * 24 * 60;
 const SYNC_QUEUE_NAME = "syncQueue";
-
-// Biting the bullet and hardcoding server host name 
-// - Workbox cannot take a regex expression for third party requests, must take full URL 
-// - Can't use enviornment variables, DOM, or importing modules to get information into service worker 
-// TODO: Must change SERVER_HOST for production to "https://cmpt373-1211-03.cmpt.sfu.ca"
-const SERVER_HOST = "http://localhost:8080";
+// Matching based on /api/v1/ in order to work with both production and localhost.
+// Might encounter issues if making requests to other third party sites as it will match
+// any URL with /api/v1/ to cache 
+const SERVER_HOST_REGEX = ".*/api/v1/";
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
@@ -26,7 +24,7 @@ const backgroundSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin(
 );
 
 workbox.routing.registerRoute(
-    new RegExp(SERVER_HOST),
+    new RegExp(SERVER_HOST_REGEX),
     new workbox.strategies.StaleWhileRevalidate({
         cacheName: "requests",
         plugins: [
@@ -47,7 +45,7 @@ workbox.routing.registerRoute(
 
 // Cannot specify a route with POST, PUT and DELETE 
 workbox.routing.registerRoute(
-    new RegExp(SERVER_HOST),
+    new RegExp(SERVER_HOST_REGEX),
     new workbox.strategies.NetworkOnly({
         plugins: [backgroundSyncPlugin],
     }),
@@ -55,7 +53,7 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-    new RegExp(SERVER_HOST),
+    new RegExp(SERVER_HOST_REGEX),
     new workbox.strategies.NetworkOnly({
         plugins: [backgroundSyncPlugin],
     }),
@@ -63,7 +61,7 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-    new RegExp(SERVER_HOST),
+    new RegExp(SERVER_HOST_REGEX),
     new workbox.strategies.NetworkOnly({
         plugins: [backgroundSyncPlugin],
     }),
