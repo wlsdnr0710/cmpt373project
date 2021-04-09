e418c32d47878e1d0c96cd93803811f8e8674edb
Yi Ching Chou <ycchou@sfu.ca>
2021-04-09 13:25:37 -0700

    Add create survey button to home page

--- cbr_front_end/src/containers/HomeNavigation/index.js
+++ cbr_front_end/src/containers/HomeNavigation/index.js
@@ -10,6 +10,7 @@ import newReferralIcon from "../../assets/svg/navigation_icons/user_pin.svg";
 import allClientsIcon from "../../assets/svg/navigation_icons/people.svg";
 import syncIcon from "../../assets/svg/navigation_icons/cloud-sync.svg";
 import logoutIcon from "../../assets/svg/navigation_icons/logout.svg";
+import surveyIcon from "../../assets/svg/navigation_icons/survey.svg";
 import "./style.css";
 
 const HomeNavigation = () => {
@@ -57,6 +58,13 @@ const HomeNavigation = () => {
                         query="clients"
                         iconSource={allClientsIcon}
                         iconAlt="All Clients"
+                    />
+                    <NavigationBarEntry
+                        label="Create Survey"
+                        destination="/new-survey"
+                        query="#"
+                        iconSource={surveyIcon}
+                        iconAlt="Create Survey"
                     />
                     <div className="sign-out" onClick={() => {removeToken()}}>
                         <NavigationBarEntry
