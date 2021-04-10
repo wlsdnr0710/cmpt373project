6e858eea3be7c96dbff49c403894f66cea699905
William Tjandra <wntjandr@sfu.ca>
2021-04-09 23:02:05 -0700

    Finished refactoring and adding edit form for clients

---
+++ cbr_front_end/src/components/WorkerInfoCard/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Badge from 'react-bootstrap/Badge';
+import ClientInfoCardAttribute from "../../components/ClientInfoCardAttribute";
+import { useHistory } from "react-router-dom";
+import avatar from "../../assets/avatar.png";
+import "./style.css";
+
+const WorkerInfoCard = ({ worker }) => {
+    const history = useHistory();
+    const onClickHandlerAllWorkers = () => {
+        history.push("/worker-information?id=" + worker.id);
+        window.scrollTo(0, 0);
+    }
+
+    return (
+        <div className="worker-info-card" onClick={onClickHandlerAllWorkers}>
+            <div className="photo-id-display">
+                <div className="worker-photo">
+                    <img src={avatar} alt="" />
+                </div>
+                <div className="id-badge-container">
+                    <Badge variant="info">
+                        <div>ID: {worker.id}</div>
+                    </Badge>
+                </div>
+            </div>
+
+            <div className="worker-info">
+                <div className="section">
+                    <ClientInfoCardAttribute keyText="First Name" valueText={worker.firstName} />
+                    <ClientInfoCardAttribute keyText="Last Name" valueText={worker.lastName} />
+                    <ClientInfoCardAttribute keyText="Role" valueText={worker.role} />
+                </div>
+                <div className="section">
+                    <ClientInfoCardAttribute keyText="Phone" valueText={worker.phone} />
+                    <ClientInfoCardAttribute keyText="Email" valueText={worker.email} />
+                    <ClientInfoCardAttribute keyText="Location" valueText={worker.zoneName.name} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WorkerInfoCard;
