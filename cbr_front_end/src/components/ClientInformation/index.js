26ec1e52e32945a49d6f5fcd5fa14c1368b0ef9c
William Tjandra <wntjandr@sfu.ca>
2021-04-09 23:46:57 -0700

    merged

--- cbr_front_end/src/components/ClientInformation/index.js
+++ cbr_front_end/src/components/ClientInformation/index.js
@@ -3,19 +3,11 @@ import defaultPhoto from "../../assets/avatar.png";
 import "./style.css";
 
 const ClientPhoto = ({ photoSource }) => {
-<<<<<<< HEAD
   if(isPhotoSourceValid()) {
     return <img className = "client-information-image" src={photoSource} alt="client"/>
   } else {
     return <img className = "client-information-image" src={defaultPhoto} alt="client"/>
   }
-=======
-    if(isPhotoSourceValid()){
-        return<img className = "client-information-image" src={photoSource} alt="client"/>
-    } else {
-        return <img className = "client-information-image" src={defaultPhoto} alt="client"/>
-    }
->>>>>>> master
 };
 
 const isPhotoSourceValid = photoSource => {
