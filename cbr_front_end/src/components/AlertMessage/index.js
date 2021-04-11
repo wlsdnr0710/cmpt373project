0bf467ec5daa05d7b851f6b35eff19c019fa3b66
jinwookk <jinwook_kim@sfu.ca>
2021-04-11 11:43:54 -0700

    Semicolons

--- cbr_front_end/src/components/AlertMessage/index.js
+++ cbr_front_end/src/components/AlertMessage/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
 import "./style.css";
-import Warning from "../../assets/svg/alert_icons/danger.svg"
-import Danger from "../../assets/svg/alert_icons/emergency.svg"
+import Warning from "../../assets/svg/alert_icons/danger.svg";
+import Danger from "../../assets/svg/alert_icons/emergency.svg";
 import { displayIcon } from '../../utils/Utilities';
 
 const AlertMessage = ({message, date, variant}) => {
